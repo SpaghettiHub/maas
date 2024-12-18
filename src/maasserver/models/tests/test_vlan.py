b14a408ccbc58e6353fbad9b4788a8b0a59399cb
r00tabot <r00tabot@gmail.com>
2024-12-18 08:16:39 +0000

    enjoy this from r00ta

--- src/maasserver/models/tests/test_vlan.py
+++ src/maasserver/models/tests/test_vlan.py
@@ -6,87 +6,104 @@ import random
 from django.core.exceptions import ValidationError
 from django.db.models import ProtectedError
 
+from maascommon.workflows.dhcp import (
+    CONFIGURE_DHCP_WORKFLOW_NAME,
+    ConfigureDHCPParam,
+)
 from maasserver.enum import INTERFACE_TYPE
+from maasserver.models import vlan as vlan_module
 from maasserver.models.interface import PhysicalInterface, VLANInterface
 from maasserver.models.notification import Notification
 from maasserver.models.vlan import VLAN
 from maasserver.testing.factory import factory
 from maasserver.testing.testcase import MAASServerTestCase
-from maasserver.utils.orm import reload_object
+from maasserver.utils.orm import post_commit_hooks, reload_object
 
 
 class TestVLANManager(MAASServerTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patch(vlan_module, "start_workflow")
+
     def test_default_specifier_matches_vid(self):
         # Note: this is for backward compatibility with the previous iteration
         # of constraints, which used vlan:<number> to mean VID, not represent
         # a database ID.
-        factory.make_VLAN()
-        vlan = factory.make_VLAN()
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            vlan = factory.make_VLAN()
+            factory.make_VLAN()
         vid = vlan.vid
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("%s" % vid), [vlan]
         )
 
     def test_default_specifier_matches_name(self):
-        factory.make_VLAN()
-        vlan = factory.make_VLAN(name="infinite-improbability")
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            vlan = factory.make_VLAN(name="infinite-improbability")
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("infinite-improbability"), [vlan]
         )
 
     def test_name_specifier_matches_name(self):
-        factory.make_VLAN()
-        vlan = factory.make_VLAN(name="infinite-improbability")
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            vlan = factory.make_VLAN(name="infinite-improbability")
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("name:infinite-improbability"),
             [vlan],
         )
 
     def test_vid_specifier_matches_vid(self):
-        factory.make_VLAN()
-        vlan = factory.make_VLAN()
-        vid = vlan.vid
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            vlan = factory.make_VLAN()
+            vid = vlan.vid
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("vid:%d" % vid), [vlan]
         )
 
     def test_space_specifier_matches_space_by_name(self):
-        factory.make_VLAN()
-        space = factory.make_Space()
-        vlan = factory.make_VLAN(space=space)
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            space = factory.make_Space()
+            vlan = factory.make_VLAN(space=space)
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("space:%s" % space.name), [vlan]
         )
 
     def test_space_specifier_matches_space_by_id(self):
-        factory.make_VLAN()
-        space = factory.make_Space()
-        vlan = factory.make_VLAN(space=space)
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            space = factory.make_Space()
+            vlan = factory.make_VLAN(space=space)
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("space:%s" % space.id), [vlan]
         )
 
     def test_class_specifier_matches_attached_subnet(self):
-        factory.make_VLAN()
-        vlan = factory.make_VLAN()
-        subnet = factory.make_Subnet(vlan=vlan)
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_VLAN()
+            vlan = factory.make_VLAN()
+            subnet = factory.make_Subnet(vlan=vlan)
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers("subnet:%s" % subnet.id), [vlan]
         )
 
     def test_class_specifier_matches_attached_fabric(self):
-        factory.make_Fabric()
-        fabric = factory.make_Fabric(name="rack42")
-        factory.make_VLAN()
-        vlan = factory.make_VLAN(fabric=fabric)
-        factory.make_VLAN()
+        with post_commit_hooks:
+            factory.make_Fabric()
+            fabric = factory.make_Fabric(name="rack42")
+            factory.make_VLAN()
+            vlan = factory.make_VLAN(fabric=fabric)
+            factory.make_VLAN()
         self.assertCountEqual(
             VLAN.objects.filter_by_specifiers(
                 "fabric:%s,vid:%d" % (fabric.name, vlan.vid)
@@ -94,19 +111,41 @@ class TestVLANManager(MAASServerTestCase):
             [vlan],
         )
 
+    def test_create_calls_configure_dhcp_workflow_if_dhcp_on(self):
+        with post_commit_hooks:
+            fabric = factory.make_Fabric()
+            start_workflow = self.patch(
+                vlan_module, "start_workflow"
+            )  # patching here to ignore default VLAN created with fabric
+            VLAN.objects.create(dhcp_on=False, vid=1, fabric_id=fabric.id)
+            start_workflow.assert_not_called()
+            vlan = VLAN.objects.create(
+                dhcp_on=True, vid=2, fabric_id=fabric.id
+            )
+        start_workflow.assert_called_once_with(
+            CONFIGURE_DHCP_WORKFLOW_NAME,
+            ConfigureDHCPParam(vlan_ids=[vlan.id]),
+        )
+
 
 class TestVLAN(MAASServerTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patch(vlan_module, "start_workflow")
+
     def test_delete_relay_vlan_doesnt_delete_vlan(self):
-        relay_vlan = factory.make_VLAN()
-        vlan = factory.make_VLAN(relay_vlan=relay_vlan)
-        relay_vlan.delete()
+        with post_commit_hooks:
+            relay_vlan = factory.make_VLAN()
+            vlan = factory.make_VLAN(relay_vlan=relay_vlan)
+            relay_vlan.delete()
         vlan = reload_object(vlan)
         self.assertIsNotNone(vlan)
         self.assertIsNone(vlan.relay_vlan)
 
     def test_delete_primary_rack_fails(self):
         rack = factory.make_RackController()
-        vlan = factory.make_VLAN(primary_rack=rack)
+        with post_commit_hooks:
+            vlan = factory.make_VLAN(primary_rack=rack)
         self.assertRaises(ProtectedError, rack.delete)
         vlan = reload_object(vlan)
         self.assertIsNotNone(vlan)
@@ -197,10 +236,12 @@ class TestVLAN(MAASServerTestCase):
             vlan.delete()
 
     def test_subnets_are_reconnected_when_vlan_is_deleted(self):
-        fabric = factory.make_Fabric()
-        vlan = factory.make_VLAN(fabric=fabric)
-        subnet = factory.make_Subnet(vlan=vlan)
-        vlan.delete()
+        with post_commit_hooks:
+            fabric = factory.make_Fabric()
+            vlan = factory.make_VLAN(fabric=fabric)
+            print(vlan.dhcp_on)
+            subnet = factory.make_Subnet(vlan=vlan)
+            vlan.delete()
         self.assertEqual(reload_object(subnet).vlan, fabric.get_default_vlan())
 
     def tests_creates_notification_when_no_dhcp(self):
@@ -214,11 +255,12 @@ class TestVLAN(MAASServerTestCase):
 
     def tests_deletes_notification_once_there_is_dhcp(self):
         Notification.objects.filter(ident="dhcp_disabled_all_vlans").delete()
-        # Force the notification to exist
-        vlan = factory.make_VLAN(dhcp_on=False)
-        # Now clear it.
-        vlan.dhcp_on = True
-        vlan.save()
+        with post_commit_hooks:
+            # Force the notification to exist
+            vlan = factory.make_VLAN(dhcp_on=False)
+            # Now clear it.
+            vlan.dhcp_on = True
+            vlan.save()
         self.assertFalse(
             Notification.objects.filter(
                 ident="dhcp_disabled_all_vlans"
@@ -226,9 +268,41 @@ class TestVLAN(MAASServerTestCase):
         )
 
     def test_connected_rack_controllers(self):
-        vlan = factory.make_VLAN()
-        racks = [factory.make_RackController(vlan=vlan) for _ in range(3)]
+        with post_commit_hooks:
+            vlan = factory.make_VLAN()
+            racks = [factory.make_RackController(vlan=vlan) for _ in range(3)]
         self.assertCountEqual(racks, vlan.connected_rack_controllers())
+
+    def test_update_calls_configure_dhcp_workflow(self):
+        start_workflow = self.patch(vlan_module, "start_workflow")
+        vlan = factory.make_VLAN(dhcp_on=False)
+        vlan.dhcp_on = True
+        vlan.mtu = 1800
+        with post_commit_hooks:
+            vlan.save()
+        start_workflow.assert_called_once_with(
+            CONFIGURE_DHCP_WORKFLOW_NAME,
+            ConfigureDHCPParam(vlan_ids=[vlan.id]),
+        )
+
+    def test_delete_calls_configure_dhcp_workflow(self):
+        start_workflow = self.patch(vlan_module, "start_workflow")
+        primary_rack = factory.make_RackController()
+        secondary_rack = factory.make_RackController()
+        with post_commit_hooks:
+            vlan = factory.make_VLAN(
+                dhcp_on=True,
+                primary_rack=primary_rack,
+                secondary_rack=secondary_rack,
+            )
+            vlan.delete()
+
+        start_workflow.assert_called_with(
+            CONFIGURE_DHCP_WORKFLOW_NAME,
+            ConfigureDHCPParam(
+                system_ids=[primary_rack.system_id, secondary_rack.system_id]
+            ),
+        )
 
 
 class TestVLANVidValidation(MAASServerTestCase):
